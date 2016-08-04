a5d105865b59999a82592ee8367a80fe6aad028f
yangsen <2625086962@qq.com>
2016-08-04 13:22:49 +0800

    第一版
    第一版，

---
+++ TextPlaceHolder/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TextPlaceHolder
+//
+
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TextPlaceHolder/ViewController.m
@@ -0,0 +1,37 @@
+//
+//  ViewController.m
+//  TextPlaceHolder
+//
+
+
+#import "ViewController.h"
+#import "YSTextView.h"
+
+@interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet YSTextView *textViewXIB;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    _textViewXIB.placeholder = @"这是XIBplaceholder这是XIBplaceholder这是XIBplaceholder";
+    _textViewXIB.placeholderNumberOfLine = 0;
+    _textViewXIB.placeholderColor = [UIColor redColor];
+}
+
+- (void)addTextView{
+    YSTextView *textF = [[YSTextView alloc]initWithFrame:(CGRect){{50,100},{275,80}}];
+    textF.layer.borderColor = [UIColor purpleColor].CGColor;
+    textF.layer.borderWidth = 1;
+    textF.placeholder = @"这是placeholder这是placeholder这是placeholder";
+    textF.font = [UIFont systemFontOfSize:20];
+    [self.view addSubview:textF];
+    
+    textF.placeholderAttributes = @{NSForegroundColorAttributeName:[UIColor redColor],NSFontAttributeName:[UIFont systemFontOfSize:20]};
+    textF.placeholderNumberOfLine = 0;
+}
+
+@end
